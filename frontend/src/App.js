416d0bb0a78bae6379daa11928d2be4ec3f9dbc2
adrian <eduardotrevinop@yahoo.com.mx>
2024-09-15 06:39:08 -0600

    graficas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,89 +5,97 @@ import ServiciosPage from './Servicios/ServiciosPage';
 import SideBar from './SideBar/SideBar';
 import DespensaPage from './Despensa/DespensaPage';
 import DespensaListPage from './Despensa/DespensaListPage';
+import Graficas from './Graficas/graficas';
 
 function App() {
 
-  const [page, setPage] = useState("Despensa");
+    const [page, setPage] = useState("Despensa");
 
-  let serviceE = {
-    name: "Electricidad",
-    price: 888,
-    paymentDate: "30 de Septiembre",
-    subDate: "Faltan 11 días"
-  }
+    let serviceE = {
+        name: "Electricidad",
+        price: 888,
+        paymentDate: "30 de Septiembre",
+        subDate: "Faltan 11 días"
+    }
 
-  let serviceA = {
-    name: "Agua",
-    price: 121,
-    paymentDate: "30 de Septiembre",
-    subDate: "Faltan 11 días"
-  }
+    let serviceA = {
+        name: "Agua",
+        price: 121,
+        paymentDate: "30 de Septiembre",
+        subDate: "Faltan 11 días"
+    }
 
-  let serviceG = {
-    name: "Gas",
-    price: 345,
-    paymentDate: "30 de Septiembre",
-    subDate: "Faltan 11 días"
-  }
+    let serviceG = {
+        name: "Gas",
+        price: 345,
+        paymentDate: "30 de Septiembre",
+        subDate: "Faltan 11 días"
+    }
 
-  let serviceI = {
-    name: "Internet",
-    price: 745,
-    paymentDate: "30 de Septiembre",
-    subDate: "Faltan 11 días"
-  }
+    let serviceI = {
+        name: "Internet",
+        price: 745,
+        paymentDate: "30 de Septiembre",
+        subDate: "Faltan 11 días"
+    }
 
-  let serviceR = {
-    name: "Renta",
-    price: 957,
-    paymentDate: "30 de Septiembre",
-    subDate: "Faltan 11 días"
-  }
+    let serviceR = {
+        name: "Renta",
+        price: 957,
+        paymentDate: "30 de Septiembre",
+        subDate: "Faltan 11 días"
+    }
 
-  let changePage = (page) => {
-    setPage(page)
-  }
+    let changePage = (page) => {
+        setPage(page)
+    }
 
-  switch (page) {
-    case "DespensaList":
-      return (
-        <div className="App">
-          <SideBar changePage={changePage}/>
-          <DespensaListPage seeItem={changePage} />
-        </div>
-      )
+    switch (page) {
+        case "DespensaList":
+            return (
+                <div className="App">
+                    <SideBar changePage={changePage} />
+                    <DespensaListPage seeItem={changePage} />
+                </div>
+            )
 
-    case "DespensaItem":
-    return (
-      <div className="App">
-        <SideBar changePage={changePage}/>
-        <DespensaPage/>
-      </div>
-    )
-    case "Servicios":
-      return (
-        <div className="App">
-          <SideBar changePage={changePage}/>
-          <ServiciosPage />
-        </div>
-      )
-    case "Despensa":
-      return (
-        <div className="App">
-          <SideBar changePage={changePage}/>
-          <ServiciosPage/>
-        </div>
-      )
-    default:
-      return (
-        
-        <div className="App">
-          <SideBar changePage={changePage}/>
-          <ServiciosPage />
-        </div>
-      )
-  }
+        case "DespensaItem":
+            return (
+                <div className="App">
+                    <SideBar changePage={changePage} />
+                    <DespensaPage />
+                </div>
+            )
+        case "Servicios":
+            return (
+                <div className="App">
+                    <SideBar changePage={changePage} />
+                    <ServiciosPage />
+                </div>
+            )
+        case "Despensa":
+            return (
+                <div className="App">
+                    <SideBar changePage={changePage} />
+                    <ServiciosPage />
+                </div>
+            )
+        case "Graficas":
+            return (
+                <div className="App">
+                    <SideBar changePage={changePage} />
+                    <Graficas />
+                </div>
+            )
+        default:
+            return (
+
+                <div className="App">
+                    <SideBar changePage={changePage} />
+                    <ServiciosPage />
+                </div>
+            )
+    }
 }
 
 export default App;
--- frontend/src/SideBar/SideBar.js
+++ frontend/src/SideBar/SideBar.js
@@ -6,23 +6,28 @@ import tasksIcon from '../Assets/tasksIcon.svg';
 import logo from '../Assets/LogoWhite.svg';
 import './SideBar.css';
 
-const SideBar = ({changePage}) => {
+const SideBar = ({ changePage }) => {
     return (
         <div className="SideBar">
-            <img src={logo} className='Logo'/>
+            <img src={logo} className='Logo' />
             {/* <h1 className='SideBar-title'>Mi Hogar</h1> */}
             <div className='SideBar-divider'></div>
             <div className='SideBar-section' onClick={() => changePage("Servicios")}>
-                <img src={homeIcon} alt="Home" className='SideBar-icon'/>
+                <img src={homeIcon} alt="Home" className='SideBar-icon' />
                 <a className='SideBar-option' href="#">Servicios</a>
             </div>
             <div className='SideBar-section' onClick={() => changePage("DespensaList")}>
-                <img src={shoppingIcon} alt="Shopping" className='SideBar-icon'/>
+                <img src={shoppingIcon} alt="Shopping" className='SideBar-icon' />
                 <a className='SideBar-option' href="#">Despensa</a>
             </div>
             <div className='SideBar-section' onClick={() => changePage("Despensa")}>
-                <img src={tasksIcon} alt="Tasks" className='SideBar-icon'/>
+                <img src={tasksIcon} alt="Tasks" className='SideBar-icon' />
                 <a className='SideBar-option' href="#">Deberes</a>
+            </div>
+
+            <div className='SideBar-section' onClick={() => changePage("Graficas")}>
+                <img src={tasksIcon} alt="Tasks" className='SideBar-icon' />
+                <a className='SideBar-option' href="#">Graficas</a>
             </div>
         </div>
     );
